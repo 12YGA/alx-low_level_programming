b09a04fcf94542d364a9cc69e73bc53bc8d5441c
12YGA <imraan2031@gmail.com>
2022-07-01 14:33:12 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,36 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - prints out all the numbers between 00 and 99
- * with no two digits being the same
+ * main - Entry point
+ *
  * Return: 0
  */
 int main(void)
 {
-	int i, j;
+	char a = 'a';
 
-	for (i = 48; i < 58; i++)
+	while (a <= 'z')
 	{
-		for (j = i; j < 58; j++)
-		{
-			if (i == j)
-			{
-				continue;
-			}
-
-			putchar(i);
-			putchar(j);
-
-			if (i == 56 && j == 57)
-			{
-				break;
-			}
-			else
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+		putchar(a);
+		a++;
 	}
+
 	putchar('\n');
 	return (0);
 }
